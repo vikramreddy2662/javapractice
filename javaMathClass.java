ecb3d9c04bbf0412aac2b280458b9d3a8742de33
Vikram Reddy <84522932+vikramreddy2662@users.noreply.github.com>
2024-01-05 23:40:54 +0530

    Add files via upload

---
+++ javaMathClass.java
@@ -0,0 +1,21 @@
+package JavaProblems;
+
+public class javaMathClass {
+    public static void main(String[] args){
+
+        double a=5.5;
+        double b=Math.ceil(a);
+        double c=Math.floor(a);
+
+        double e=Math.toRadians(a);
+        System.out.println(b+  ":" +c+  ":"  +Math.random());
+
+        System.out.println(+Math.sin(a));
+        System.out.println(+Math.cos(a));
+        System.out.println(+Math.tan(a));
+        System.out.println(+Math.abs(a));
+        System.out.println(+Math.addExact(44,55));
+        System.out.println(+Math.subtractExact(67,87));
+
+    }
+}
