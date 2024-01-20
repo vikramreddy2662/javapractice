849181d1dbf525ccbae539c0f2c1abd2d962e456
Vikram Reddy <84522932+vikramreddy2662@users.noreply.github.com>
2024-01-20 22:24:47 +0530

    Add files via upload

---
+++ conversions2.java
@@ -0,0 +1,28 @@
+package JavaPractice;
+
+public class conversions2 {
+    public static void main (String[] args){
+        int i=12;
+        String s=String.valueOf(i);
+        System.out.println(s+23);
+        int a=Integer.parseInt(s);
+        System.out.println(a+45);
+        double d=12.3;
+        String s1=String.valueOf(d);
+        System.out.println(s1+23);
+        System.out.println(s1+344);
+
+        int b=34;
+        Integer c=b;
+        System.out.println(c);
+
+        double d1=5.5;
+        int g=(int)d1;
+        System.out.println(g);
+
+
+
+
+
+    }
+}
